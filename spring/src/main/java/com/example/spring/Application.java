ed33fe2c4012244c4339a1d9c81dff993f0aaa4a
lmalheiro7 <lmalheiro@ipvc.pt>
2022-06-23 15:53:01 +0100

    upload

--- spring/src/main/java/com/example/spring/Application.java
+++ spring/src/main/java/com/example/spring/Application.java
@@ -1,6 +1,5 @@
 package com.example.spring;
 
-import com.example.bd.BLL.ClienteBLL;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
